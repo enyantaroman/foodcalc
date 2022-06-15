024ff8ba27d705f782d0d73cb101450d8ca1bd7a
kashii shintaro <yantaro@yanmba.local>
2022-06-15 22:43:08 +0900

    frontend setup basic

---
+++ frontend/src/views/pages/Home.js
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <Container maxWidth="md">
+        <h1>ホーム</h1>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
